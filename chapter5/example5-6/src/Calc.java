1fdca5f70dca1214afa836d85eae0c48b56885ed
AngryOvO <101005580+AngryOvO@users.noreply.github.com>
2022-10-18 14:12:31 +0900

    Delete chapter5 directory

--- chapter5/example5-6/src/Calc.java
+++
@@ -1,8 +0,0 @@
-public abstract class Calc { // �߻� Ŭ����
-    protected int a, b;
-    public void setValue(int a, int b) {
-        this.a = a;
-        this.b = b;
-    }
-    public abstract int calculate(); // �߻� �޼ҵ�. ���� Ŭ�������� ������ ����
-}
--- chapter5/example5-7/src/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main
-{
-    public static void main(String[] args)
-    {
-        Shape dounut = new Circle(10);
-        dounut.redraw();
-        System.out.println("면적은 " + dounut.getArea());
-    }
-}
